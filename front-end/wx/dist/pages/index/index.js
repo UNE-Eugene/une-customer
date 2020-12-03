7b7023095876646293298ba2427365f36faaec3a
UNE-Eugene <Eugene@une.ltd>
2020-12-03 11:46:43 +0800

    微信小程序登录页面更新

--- front-end/wx/dist/pages/index/index.js
+++ front-end/wx/dist/pages/index/index.js
@@ -604,7 +604,7 @@ __webpack_require__.r(__webpack_exports__);
 /***/ (function(module, exports, __webpack_require__) {
 
 // extracted by mini-css-extract-plugin
-module.exports = {"app":"app___2lhPP","header":"header___3qOk9","background":"background___ZSAAH","card":"card___3O7TJ","logo":"logo___Al4Xb","title":"title___2mr8X","button":"button___3NgDU"};
+module.exports = {"app":"app___2lhPP","header":"header___3qOk9","background":"background___ZSAAH","card":"card___3O7TJ","logo":"logo___Al4Xb","title":"title___2mr8X","username":"username___1NxQQ","password":"password___2icBm","placeholder":"placeholder___1o4QS","button":"button___3NgDU"};
 
 /***/ }),
 
@@ -641,6 +641,10 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _index_css_modules__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_index_css_modules__WEBPACK_IMPORTED_MODULE_2__);
 /* harmony import */ var _static_bg_png__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../static/bg.png */ "./src/pages/static/bg.png");
 /* harmony import */ var _public_logo_png__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../../../public/logo.png */ "./public/logo.png");
+/* harmony import */ var _static_icon1_png__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../static/icon1.png */ "./src/pages/static/icon1.png");
+/* harmony import */ var _static_icon2_png__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ../static/icon2.png */ "./src/pages/static/icon2.png");
+
+
 
 
 
@@ -661,7 +665,23 @@ __webpack_require__.r(__webpack_exports__);
     className: _index_css_modules__WEBPACK_IMPORTED_MODULE_2___default.a.card
   }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__["createElement"](remax_wechat__WEBPACK_IMPORTED_MODULE_1__["Text"], {
     className: _index_css_modules__WEBPACK_IMPORTED_MODULE_2___default.a.title
-  }, "\u767B \u5F55")));
+  }, "\u767B \u5F55"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("view", {
+    className: _index_css_modules__WEBPACK_IMPORTED_MODULE_2___default.a.username
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__["createElement"](remax_wechat__WEBPACK_IMPORTED_MODULE_1__["Image"], {
+    src: _static_icon1_png__WEBPACK_IMPORTED_MODULE_5__["default"]
+  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__["createElement"](remax_wechat__WEBPACK_IMPORTED_MODULE_1__["Input"], {
+    placeholder: "\u7528\u6237\u540D",
+    placeholderClassName: _index_css_modules__WEBPACK_IMPORTED_MODULE_2___default.a.placeholder
+  })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("view", {
+    className: _index_css_modules__WEBPACK_IMPORTED_MODULE_2___default.a.password
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__["createElement"](remax_wechat__WEBPACK_IMPORTED_MODULE_1__["Image"], {
+    src: _static_icon2_png__WEBPACK_IMPORTED_MODULE_6__["default"]
+  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__["createElement"](remax_wechat__WEBPACK_IMPORTED_MODULE_1__["Input"], {
+    placeholder: "\u5BC6\u7801",
+    placeholderClassName: _index_css_modules__WEBPACK_IMPORTED_MODULE_2___default.a.placeholder
+  })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__["createElement"](remax_wechat__WEBPACK_IMPORTED_MODULE_1__["Button"], {
+    className: _index_css_modules__WEBPACK_IMPORTED_MODULE_2___default.a.button
+  }, "\u767B\u5F55")));
 });
 
 /***/ }),
@@ -676,6 +696,32 @@ __webpack_require__.r(__webpack_exports__);
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony default export */ __webpack_exports__["default"] = (__webpack_require__.p + "6316091b4cf4c16062d83fb2c05a7aeb.png");
+
+/***/ }),
+
+/***/ "./src/pages/static/icon1.png":
+/*!************************************!*\
+  !*** ./src/pages/static/icon1.png ***!
+  \************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = (__webpack_require__.p + "2a3b490cd6aecdfa52c8fc278e10f0d5.png");
+
+/***/ }),
+
+/***/ "./src/pages/static/icon2.png":
+/*!************************************!*\
+  !*** ./src/pages/static/icon2.png ***!
+  \************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = (__webpack_require__.p + "aa90e1febb9f25a49e4e70b1aa503fe1.png");
 
 /***/ }),
 
--- front-end/wx/src/pages/index/index.js
+++ front-end/wx/src/pages/index/index.js
@@ -1,10 +1,10 @@
 import * as React from 'react';
-import { View, Text, Image, Button } from 'remax/wechat';
+import { View, Text, Image, Button, Input } from 'remax/wechat';
 import styles from './index.css';
 import Background from '../static/bg.png'
 import Logo from '../../../public/logo.png'
-
-
+import UserLogo from '../static/icon1.png'
+import PassLogo from '../static/icon2.png'
 export default () => {
   return (
     <View className={styles.app}>
@@ -14,6 +14,15 @@ export default () => {
       </View>
       <View className={styles.card}>
         <Text className={styles.title}>登 录</Text>
+        <view className={styles.username}>
+          <Image src={UserLogo} ></Image>
+          <Input placeholder='用户名' placeholderClassName={styles.placeholder}/>
+        </view>
+        <view className={styles.password}>
+          <Image src={PassLogo} ></Image>
+          <Input placeholder='密码' placeholderClassName={styles.placeholder}/>
+        </view>
+        <Button className={styles.button}>登录</Button>
       </View>
     </View>
   );
